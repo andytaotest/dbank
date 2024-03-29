62174c2f2f0705aa95bf48e4f868e0722e644bcd
andytaotest <andytaotest@gmail.com>
2024-03-29 11:37:58 +0800

    initial commit

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,53 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Int "mo:base/Int";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentValue: Float = 300; // With "stable" keyword, it's stable/orthogonal persisted variable
+  currentValue := 300;
+  Debug.print(debug_show(currentValue));
+
+  stable var startTime = Time.now();
+  startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  let id = 12567890; // let for constant, never change
+  // Debug.print("Hello");
+  // Debug.print(debug_show(id));
+
+  public func topUp(amount: Float) { 
+    // Without the "public" keyword, it's private method, only accessible in the class.
+    // This's the "update" method since no "query" keyword, which updates canister status, hence more time consuming.
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+  // topUp();
+
+  public func withdraw(amount: Float) {
+    // Nat is natural number which's a positive whole number, sometimes with a inclusion of zero.
+    // Nat is a subtype of Int.
+    let tempValue: Float = currentValue - amount;
+    if (tempValue >= 0) {
+      currentValue -= amount;
+      Debug.print(debug_show(currentValue));
+    } else {
+      Debug.print("The amount to deduct is bigger than the current value.");
+    }
+  };
+
+  // This is the "query" function, which is faster since no changing blockchain.
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS)); // assume 1% interest per second
+    startTime := currentTime;
+  };
+
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,41 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async function() {
+  // console.log("Finished loading.");
+  update();
 });
+
+document.querySelector("form").addEventListener("submit", async function() {
+  // console.log("Submitted.");
+  // Get rid of the default button behaviour while submitting
+  event.preventDefault();
+  // Get a handler of the button
+  const button = event.target.querySelector("#submit-btn"); // event.target is to get the form
+  // Get the values on the screen
+  const topUpAmount = parseFloat(document.getElementById("input-amount").value);
+  const withdrawAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+  // Disable the button
+  button.setAttribute("disabled", true);
+  // Top up
+  if (document.getElementById("input-amount").value.length != 0) {
+    await dbank.topUp(topUpAmount);
+  }
+  // Withdraw
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await dbank.withdraw(withdrawAmount);
+  }
+  // Calculate compound
+  await dbank.compound();
+  // Display the latest balance 
+  update();
+  // Clear the fields
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+  // Enable the button
+  button.removeAttribute("disabled");
+});
+
+async function update() {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerHTML = currentAmount.toFixed(2);
+};
